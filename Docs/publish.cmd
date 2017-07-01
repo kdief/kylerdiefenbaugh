8264c3e6a3583c05abbb72d9623deecf64f31afa
kdief <diefenk@gmail.com>
2017-06-30 19:56:21 -0700

    Testing automated publish

---
+++ Docs/publish.cmd
@@ -0,0 +1,9 @@
+git-checkout gh-pages     # going to the gh-pages branch
+
+git rebase master         # sync gh-pages with master 
+
+git push origin gh-pages  # commit changes to gh-pages
+
+git checkout master       # return to the master
+
+pause
