cfc9f1d9042832fa5cbb37b82f57e1d22d60036f
kdief <diefenk@gmail.com>
2017-06-30 20:05:04 -0700

    Updated cmd file

--- Docs/publish.cmd
+++ Docs/publish.cmd
@@ -1,6 +1,24 @@
+@echo off
+
+echo 'Changing to parent directory'
 cd ..
+pause
+
+echo 'Switching to gh-pages branch'
 git checkout gh-pages
-git rebase master
+pause
+
+echo 'Syncing with master branch'
+git rem rebase master
+pause
+
+echo 'Pushing to remote gh-pages branch'
 git push origin gh-pages
+pause
+
+echo 'Switching back to master branch'
 git checkout master
+pause
+
+echo 'Done'
 pause
