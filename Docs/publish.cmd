1ff955d195ff25dee8600e3b50033db994dec8bd
kdief <diefenk@gmail.com>
2017-06-30 19:57:47 -0700

    Updated script

--- Docs/publish.cmd
+++ Docs/publish.cmd
@@ -1,9 +1,5 @@
-git-checkout gh-pages     # going to the gh-pages branch
-
-git rebase master         # sync gh-pages with master 
-
-git push origin gh-pages  # commit changes to gh-pages
-
-git checkout master       # return to the master
-
+git checkout gh-pages
+git rebase master
+git push origin gh-pages
+git checkout master
 pause
