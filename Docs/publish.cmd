ae1034db4ac15777016aac09bfacce0d17a6c1ee
kdief <diefenk@gmail.com>
2017-06-30 23:25:52 -0700

    test

--- Docs/publish.cmd
+++ Docs/publish.cmd
@@ -1,24 +1,27 @@
 @echo off
 
-echo 'Changing to parent directory'
+echo *** Make sure changes are committed and pushed to master branch ***
+pause
+
+echo *** Changing to parent directory ***
 cd ..
 pause
 
-echo 'Switching to gh-pages branch'
-git checkout gh-pages
+echo *** Switching to gh-pages branch ***
+call git checkout gh-pages
 pause
 
-echo 'Syncing with master branch'
-git rem rebase master
+echo *** Syncing with master branch ***
+rem git.exe rebase master
 pause
 
-echo 'Pushing to remote gh-pages branch'
-git push origin gh-pages
+echo *** Pushing to remote gh-pages branch ***
+rem git.exe push origin gh-pages
 pause
 
-echo 'Switching back to master branch'
-git checkout master
+echo *** Switching back to master branch ***
+rem git.exe checkout master
 pause
 
-echo 'Done'
+echo *** Done **
 pause
